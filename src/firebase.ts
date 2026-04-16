c1fd377bd1d57df52c8ce645c0fc54d43b38ba05
scolee1972 <scolee1972@gmail.com>
2026-04-16 15:07:20 +0900

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,7 @@
+export const isConfigured = false;
+export const db = null;
+export const collection = (..._a: any[]) => null;
+export const doc = (..._a: any[]) => null;
+export const setDoc = async (..._a: any[]) => {};
+export const getDocs = async (..._a: any[]) => ({ forEach: () => {} });
+export const deleteDoc = async (..._a: any[]) => {};
